d64a35ac67bc5fda69d0463d4bd33714393e7154
tianpengyu <pengyutian@yangqianguan.com>
2020-06-23 15:40:37 +0800

    init

---
+++ src/main/java/org/baymax/algorithmlearn/AlgorithmLearnApplication.java
@@ -0,0 +1,13 @@
+package org.baymax.algorithmlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmLearnApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AlgorithmLearnApplication.class, args);
+  }
+
+}
