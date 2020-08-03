3ef763a2dc0233aa324b1879d7b3f6effe587fa0
tianpengyu <pengyutian@yangqianguan.com>
2020-08-03 23:41:35 +0800

    二叉树公共祖先

--- src/main/java/org/baymax/interview/algorithm/recursion/RecursionDemo.java
+++ src/main/java/org/baymax/interview/algorithm/recursion/RecursionDemo.java
@@ -10,7 +10,12 @@ package org.baymax.interview.algorithm.recursion;
  *   94
  *   154
  *
- * 3.
+ * 3. 树形问题
+ *
+ * 使用递归算法时 重要的思想：回溯法，可以应用在树形问题上
+ * 解决这个问题的思路 本质是一个树的形状
+ *
+ *
  * @author tianpengyu
  * @version 1.0
  * @date 2020/8/1 09:56
